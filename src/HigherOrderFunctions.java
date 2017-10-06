a88de911e0171c6cd5ff805fb1d20ebf79ff9f9c
Casper12345 <caspertellerup@hotmail.com>
2017-10-06 12:23:44 +0100

    First Commit

--- src/HigherOrderFunctions.java
+++ src/HigherOrderFunctions.java
@@ -2,7 +2,16 @@ import java.util.List;
 
 public class HigherOrderFunctions {
 
-
+  /**
+   * Implementation of map function.
+   *
+   * @param lst list of generic type A
+   * @param returnList list of generic type B
+   * @param function functional interface
+   * @param <A> generic type A
+   * @param <B> generic type B
+   * @return list of B.
+   */
   public <A, B> List<B> map(List<A> lst, List<B> returnList, Function<A, B> function) {
     if (lst.isEmpty()) {
       return returnList;
@@ -16,6 +25,12 @@ public class HigherOrderFunctions {
 
 }
 
+/**
+ * Functional interface with simple abstract method.
+ *
+ * @param <A> generic param
+ * @param <B> generic param
+ */
 interface Function<A, B> {
 
   B apply(A a);
